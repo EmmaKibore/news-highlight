ae16ff052f53ed71b524fdab5bf15baf9515264e
EmmaKibore <emmakibore@gmail.com>
2019-04-15 01:14:18 +0300

    made indentation corrections in error.py

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,6 +1,9 @@
+from flask import render_template
+from . import main
+
 @main.app_errorhandler(404)
-def four_0_four(error)
-'''
-Function to render the 404 page
-'''
-return render _template('four_of_four.html'),404
+def four_Ow_four(error)
+    '''
+    Function to render the 404 page
+    '''
+    return render _template('four_Ow_four.html'),404
