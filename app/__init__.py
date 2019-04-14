e15950a2bf27fe7faff6bbcd806934fc8f9750ba
EmmaKibore <emmakibore@gmail.com>
2019-04-14 19:28:21 +0300

    registered blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,32 @@
+from config import DevConfig
+from config import config_options
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import config_options
 
 bootstrap = Bootstrap()
 
 def create_app(config_name):
+    #We create a create_app() function that takes the configuration setting key as an argument. 
+    
+# Initializing application
     app = Flask(__name__)
 
-    # Creating app configurations
+
+     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    # Initializing Flask Extensions
+    # Initializing flask extensions
     bootstrap.init_app(app)
 
-    # Registering the BluePrint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    # Will add the views and forms
 
-    # setting the config
+     # setting config
     from .request import configure_request
     configure_request(app)
 
-    return app
+    return app   
+
+
